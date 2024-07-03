8b131fbde1b592d936f589e749732f16027e5fa5
Mukengeshayi <desiremukengeshayi27@gmail.com>
2024-07-03 04:47:50 -0800

    update all migrations

--- database/migrations/2024_07_03_115720_create_posts_table.php
+++ database/migrations/2024_07_03_115720_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();;
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
